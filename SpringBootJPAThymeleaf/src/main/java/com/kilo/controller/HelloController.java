4ce9fe2bbc5a06c687d5b4a1c8de628f3accfb79
knightlylee <642026443@qq.com>
2018-04-28 15:09:07 +0800

    thymeleaf

---
+++ SpringBootJPAThymeleaf/src/main/java/com/kilo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.kilo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by kilo on 2018/4/28.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("message", "http://gitbook.cn");
+        return "hello";
+    }
+
+}
