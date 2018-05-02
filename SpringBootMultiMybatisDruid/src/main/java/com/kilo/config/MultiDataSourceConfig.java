c6a629b744b0e609225aef2e16db865d789ecf75
knightlylee <642026443@qq.com>
2018-05-02 08:51:22 +0800

    spring boot整合MyBatis,druid,多数据源

--- SpringBootMultiMybatisDruid/src/main/java/com/kilo/config/MultiDataSourceConfig.java
+++ SpringBootMultiMybatisDruid/src/main/java/com/kilo/config/MultiDataSourceConfig.java
@@ -1,6 +1,6 @@
 package com.kilo.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +19,13 @@ public class MultiDataSourceConfig {
     @Bean("oneDataSource")
     @ConfigurationProperties("spring.datasource.druid.one")
     public DataSource dataSourceOne() {
-        return new DruidDataSource();
+
+        return DruidDataSourceBuilder.create().build();
     }
 
     @Bean("twoDataSource")
     @ConfigurationProperties("spring.datasource.druid.two")
     public DataSource dataSourceTwo() {
-        return new DruidDataSource();
+        return DruidDataSourceBuilder.create().build();
     }
 }
--- SpringBootMultiMybatisDruid/src/main/java/com/kilo/config/OneDataSourceConfig.java
+++ SpringBootMultiMybatisDruid/src/main/java/com/kilo/config/OneDataSourceConfig.java
@@ -19,6 +19,13 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = "com.kilo.mapper.one",sqlSessionTemplateRef = "oneSqlSessionTemplate")
 public class OneDataSourceConfig {
 
+    /**
+     * 创建一个SqlSessionFactory，
+     * 将第一个数据源注入到其中
+     * @param dataSource
+     * @return
+     * @throws Exception
+     */
     @Primary
     @Bean("oneSqlSessionFactory")
     public SqlSessionFactory oneSqlSessionFactory(@Qualifier("oneDataSource") DataSource dataSource) throws Exception {
@@ -27,12 +34,24 @@ public class OneDataSourceConfig {
         return bean.getObject();
     }
 
+    /**
+     * 创建事务管理
+     * 将数据源添加到事务中
+     * @param dataSource
+     * @return
+     */
     @Primary
     @Bean("oneTransactionManager")
     public DataSourceTransactionManager oneTransactionManager(@Qualifier("oneDataSource") DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 
+    /**
+     * 创建在 Mapper 中需要使用的SqlSessionTemplate
+     * 将上面创建的SqlSessionFactory注入
+     * @param sqlSessionFactory
+     * @return
+     */
     @Primary
     @Bean("oneSqlSessionTemplate")
     public SqlSessionTemplate oneSqlSessionTemplate(@Qualifier("oneSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
