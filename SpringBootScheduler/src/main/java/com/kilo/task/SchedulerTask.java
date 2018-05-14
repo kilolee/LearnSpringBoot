97c12b5af47e542293789bd0e1f916ce3e21b094
knightlylee <642026443@qq.com>
2018-05-14 21:11:03 +0800

    SpringBoot内置的方式使用注解来实现定时任务

---
+++ SpringBootScheduler/src/main/java/com/kilo/task/SchedulerTask.java
@@ -0,0 +1,22 @@
+package com.kilo.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by kilo on 2018/5/14.
+ */
+@Component
+public class SchedulerTask {
+
+    private int count=0;
+
+    @Scheduled(cron = "*/6 * * * * ?")
+    private void process(){
+        System.out.println("this is scheduler task runing  "+(count++));
+    }
+
+
+
+
+}
